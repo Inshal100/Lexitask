a06eae1d9b5eff2e95d22ad1f02cc29a8a86550e
Inshal00 <inshalamir00@gmail.com>
2024-10-16 23:48:37 +0500

    Native loading page

--- lib/Loading.dart
+++ lib/Loading.dart
@@ -23,15 +23,26 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
         child: Expanded(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Image.asset('assets/images/logo.jpeg', height: 300, width: 300),
             Text(
               "Loading",
               style: TextStyle(fontSize: 30),
+            ),
+            TweenAnimationBuilder<Color?>(
+              tween: ColorTween(begin: Colors.blue, end: Colors.red),
+              duration: Duration(seconds: 2),
+              builder: (context, color, child) {
+                return CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(color!),
+                );
+              },
             ),
           ],
         )),
