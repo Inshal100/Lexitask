ea43445b95a218e07e8111df4e1ff400e57703c5
Inshal00 <inshalamir00@gmail.com>
2024-10-17 22:04:34 +0500

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:lexitask_app/home_screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print('Pausing');
+    await Future.delayed(const Duration(seconds: 2));
+    print('Unpausing');
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
